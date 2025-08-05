855ee9bef3c337e49928382280f5d7b5f4a3c2ff
Dan Armstrong <armstrongdanielc@gmail.com>
2025-08-05 01:52:07 +0100

    Added step decorator to the booking service functions

--- services/booking.service.ts
+++ services/booking.service.ts
@@ -1,5 +1,6 @@
 import { APIRequestContext, APIResponse } from '@playwright/test';
 import { TestConfig } from '../framework/config';
+import { step } from '../framework/decorators';
 
 export class BookingService {
   private readonly url = TestConfig.urls.api.useGateway ? TestConfig.urls.api.gateway : TestConfig.urls.api.bookingService;
@@ -9,22 +10,26 @@ export class BookingService {
 
   constructor(private requestContext: APIRequestContext) {}
 
+  @step('Create a Booking')
   async createBooking(bookingData: Record<string, unknown>): Promise<APIResponse> {
     return await this.requestContext.post(this.bookingEndpoint, {
       data: bookingData
     });
   }
 
+  @step(bookingId => `Get Booking by ID: ${bookingId}`)
   async getBookingById(bookingId: string): Promise<APIResponse> {
     return await this.requestContext.get(this.bookingByIdEndpoint(bookingId));
   }
 
+  @step(bookingId => `Update Booking with ID: ${bookingId}`)
   async updateBooking(bookingId: string, bookingData: Record<string, unknown>): Promise<APIResponse> {
     return await this.requestContext.patch(this.bookingByIdEndpoint(bookingId), {
       data: bookingData
     });
   }
 
+  @step(bookingId => `Delete Booking with ID: ${bookingId}`)
   async deleteBooking(bookingId: string): Promise<APIResponse> {
     return await this.requestContext.delete(this.bookingByIdEndpoint(bookingId));
   }
